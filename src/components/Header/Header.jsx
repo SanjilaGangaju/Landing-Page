a9208f482d6461e80657326a2c0622801fa65aa8
Sanjila Gangaju <canzilagangaju@gmail.com>
2025-08-19 20:47:55 +0545

    create navbar responsiveness

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Header.css"
 import Navbar from "../Navbar/Navbar"
+import { RxHamburgerMenu } from "react-icons/rx";
+import { IoMdClose } from "react-icons/io";
+
 const Header = () => {
+  const [ishamOn, sethamOn] = useState(false);
   return (
-    <header >
-        <div className='wrapper flex justify-between items-center pt-5 '>
+    <header>
+     
+        <div className='wrapper relative flex justify-between items-center pt-5 '>
             <div className='font-[roboto] text-[36px] font-bold text-[#42A7C3]'>
                 Travling!
             </div>
+           
+           
+            <button onClick={()=>sethamOn(!ishamOn)} className='md:hidden lg:hidden'>
+
+             <RxHamburgerMenu >dgdg</RxHamburgerMenu>
+
+            </button>
+             {ishamOn&&(
+                 <div className='absolute w-1/2 right-0  h-50 flex flex-col gap-2 items-center justify-center bg-blue-100 top-20 '>
+                  <IoMdClose onClick={()=>sethamOn(!ishamOn)} className='ml-[50%]'></IoMdClose>
             <Navbar></Navbar>
-            <div className='flex items-center  justify-center'>
-                <button className='rounded-md px-3 py-2 bg-[#FA8443] text-gray-50 cursor-pointer hover:bg-orange-400 font-[lato] font-bold text-[16px]'>Sign Up</button>
-            </div>
+          </div>
+             )}
+       
         </div>
 
     </header>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router'
 import "./Navbar.css"
+
 const Navbar = () => {
   return (
-   <nav className='flex gap-[80px] font-[lato] font-normal text-[17px]'>
+   <nav className='flex flex-col gap-8 items-center justify-center font-[lato] font-normal text-[20px]'>
       <Link to="/product">Product</Link>
       <Link to="/contact-us">Contact Us</Link>
       <Link to="/about-us">About Us</Link>
