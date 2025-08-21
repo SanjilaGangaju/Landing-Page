febd7e1f1735afe9b083f7b4563d3749f20ebef0
Sanjila Gangaju <canzilagangaju@gmail.com>
2025-08-21 09:57:03 +0545

    hero responsiveness

--- src/components/Destinations/Destinations.jsx
+++ src/components/Destinations/Destinations.jsx
@@ -4,14 +4,14 @@ import destinations from "./destination_detail"
 const Destinations = () => {
   return (
     <section>
-    <div className='wrapper py-[6rem] flex flex-col gap-10'>
+    <div className='wrapper py-[6rem] px-2 flex flex-col gap-10'>
         <div className='flex flex-col gap-2'>
              <h2 className='title font-bold font-[poppins] text-2xl'>Popular Destinations</h2>
            <p className='small-text text-md'>Vacations to make your experience enjoyable in Indonesia!</p>
         </div>
           
 
-      <div className='grid grid-cols-4 gap-5'>
+      <div className='grid grid-cols-2 md:grid-cols-4 gap-5'>
               {destinations.map(item=>(<Cards key={item.title} img={item.img} w="large" location={item.location} per={item.per} duration={item.duration} price={item.price}
               title={item.title}></Cards>))}
 
