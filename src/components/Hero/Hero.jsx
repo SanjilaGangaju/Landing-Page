0fca03fda4e80e04f65c110605842485a89df688
Sanjila Gangaju <canzilagangaju@gmail.com>
2025-08-22 12:03:31 +0545

    tweak components

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
       <div className="wrapper grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 justify-between gap-20 py-[2rem] lg:py-[5rem] px-2 lg:px-12 items-center">
         
         {/* LEFT SIDE (text + buttons) */}
-        <div className="flex flex-col gap-[32px] w-full sm:order-2 md:order-1 lg:order-1 order-2">
+        <div className="flex flex-col gap-[39px] w-full sm:order-2 md:order-1 lg:order-1 order-2">
           <h1 className="font-[poppins] text-[#333333] font-bold text-[36px] md:text-[56px] lg:text-[5rem]">
             Start your journey by one click, explore beautiful world!
           </h1>
@@ -17,7 +17,7 @@ const Hero = () => {
           </p>
 
           {/* STORE BUTTONS */}
-          <div className="flex gap-2 lg:gap-5 items-center justify-center">
+          <div className="flex gap-2 lg:gap-5 justify-start">
             <div className="bg-[#1d1d1d]  cursor-pointer flex items-center justify-center gap-2 py-2 px-2 hover:bg-black rounded">
               <div className="w-1/7">
                 <img className="w-full" src="/images/playstoreicon.png" />
@@ -51,7 +51,7 @@ const Hero = () => {
 
         {/* RIGHT SIDE (image + overlays) */}
         <div className="order-1 md:order-2 lg:order-2 sm:order-1">
-          <div className="relative w-full max-w-[500px] mx-auto">
+          <div className="relative w-full max-w-[500px] md:max-w-[600px] lg:max-w-[900px] mx-auto">
             {/* Background image */}
             <img
               className="w-full h-auto object-cover rounded-full"
@@ -69,7 +69,7 @@ const Hero = () => {
             </div>
 
             {/* Frame overlay */}
-            <div className="absolute top-[8%] left-[8%] w-[20%]">
+            <div className="absolute top-[8%] lg:top-[5%] lg:left-[20%]  left-[8%] ">
               <img src="/images/Frame 11.png" alt="Frame" />
             </div>
 
@@ -94,13 +94,13 @@ const Hero = () => {
             </div>
 
             {/* Flight badge */}
-            <div className="absolute top-[20%] left-[70%] md:left-[70%] bg-gray-50 flex items-center justify-center gap-1 py-1 px-2 rounded-full w-auto">
+            <div className="absolute top-[20%] left-[70%] md:left-[70%] lg:left-[80%] bg-gray-50 flex items-center justify-center gap-1 py-1 px-2 rounded-full w-[15%]">
               <img
-                className="w-4 h-4"
+                className="w-4 h-4 lg:w-5 lg:h-5"
                 src="/images/ic_baseline-flight.png"
                 alt="flight"
               />
-              <p className="text-[11px] md:text-[13px]">Jakarta</p>
+              <p className="text-[11px] md:text-[13px] lg:text-[25px]">Jakarta</p>
             </div>
           </div>
         </div>
--- src/components/Vacation/Vacation.jsx
+++ src/components/Vacation/Vacation.jsx
@@ -6,29 +6,19 @@ const Vacation = () => {
     <section>
     <div className='wrapper flex items-center flex-col  py-[2rem] lg:py-[5rem] lg:px-12 px-2'>
         <div className='flex flex-col items-center gap-3'>
-             <h2 className='title text-3xl lg:text-6xl font-bold font-[poppins] text-center'>Plan Your Vacation!</h2>
+             <h2 className='title text-3xl lg:text-5xl font-bold font-[poppins] text-center'>Plan Your Vacation!</h2>
         <p className='small-text text-[18px]  lg:text-2xl w-[80%] text-center'>For many people organizing a trip is a headache. Register to be able to jointly determine vacation destinations and dates</p>
 
         </div>
        
         <div className='h-full relative w-full flex items-center justify-center'>
-            <img className="md:w-[90%] lg:w-[70%] w-[100%] -mt-10 md:-mt-30 md:-mb-22 opacity-[0.9] md:opacity-[0.5] lg:opacity-[0.5] h-1/1 object-cover" src="/images/hexagonworld.png"></img>
-            <div className='absolute left-50 hidden  md:flex top-30 '>
-              <DragCard  src="" title="De Pavijoen Bandung" category="Hotel" span1="1 Kamar" span2="2 Orang" span3="Check-in 13:00"></DragCard>
+            <img className="md:w-[90%] lg:w-[70%] w-[100%] -mt-10 md:-mt-20 md:-mb-22 opacity-[0.9] md:opacity-[0.5] lg:opacity-[0.5] h-1/1 object-cover" src="/images/hexagonworld.png"></img>
+            <div className='absolute md:left-[8%] md:flex lg:flex lg:left-[20%] hidden md:top-[10%] lg:top-[20%]'>
+              <DragCard  src="/images/Rectangle 9 (1).png" title="De Pavijoen Bandung" category="Hotel" span1="1 Kamar" span2="2 Orang" span3="Check-in 13:00"></DragCard>
 
             </div>
-           <div className='absolute left-50 hidden lg:flex md:flex top-30'>
-  <DragCard  
-    src="/images/Rectangle 9 (1).png" 
-    title="De Pavijoen Bandung" 
-    category="Hotel" 
-    span1="1 Kamar" 
-    span2="2 Orang" 
-    span3="Check-in 13:00"
-  />
-</div>
 
-<div className='absolute right-20 top-40 hidden lg:flex md:flex'>
+<div className='absolute md:left-[50%] md:flex lg:flex lg:left-[70%] hidden md:top-[50%] lg:top-[30%]'>
   <DragCard  
     src="/images/Rectangle 9.png" 
     title="City Tour Bandung" 
@@ -39,7 +29,7 @@ const Vacation = () => {
   />
 </div>
 
-<div className='absolute left-40 bottom-70 hidden lg:flex md:flex'>
+<div className='absolute md:left-[20%] md:flex lg:flex lg:left-[20%] hidden md:top-[60%] lg:top-[50%]'>
   <DragCard  
     src="/images/Rectangle 9 (2).png" 
     title="Garuda Indonesia - Jakarta" 
@@ -50,7 +40,7 @@ const Vacation = () => {
   />
 </div>
 
-<div className='absolute right-20 hidden md:flex lg:flex bottom-70'>
+<div className='absolute md:left-[60%] md:flex lg:flex lg:left-[70%] hidden md:top-[20%] lg:top-[60%]'>
   <DragCard  
     src="/images/Rectangle 9 (3).png" 
     title="Rental Mobil Bandung" 
